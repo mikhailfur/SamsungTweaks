1819309d86b777dc16bc3eb98d581d0d9da48046
mikhailfur <admin@mikhailfur.ru>
2025-08-27 22:40:36 +0500

    fixing problems
    First try error, trying to fix

--- uninstall.sh
+++ uninstall.sh
@@ -7,13 +7,11 @@
 ORIG_FILE=/system/etc/floating_feature.xml
 
 # Путь к резервной копии внутри модуля.
-# Magisk сохраняет данные модуля в /data/adb/modules/ID_МОДУЛЯ
-# Переменная $MODDIR указывает на эту директорию
+# Переменная $MODDIR указывает на директорию модуля в /data/adb/modules/
 MOD_BACKUP=$MODDIR/system/etc/floating_feature.xml.bak
 
 # Проверяем, существует ли наша резервная копия
 if [ -f "$MOD_BACKUP" ]; then
-  # Восстанавливаем оригинальный файл из бэкапа
-  # Флаг -Z нужен для восстановления контекста SELinux
-  cp -aZ "$MOD_BACKUP" "$ORIG_FILE"
+  # Восстанавливаем оригинальный файл из бэкапа (БЕЗ флага -Z)
+  cp -a "$MOD_BACKUP" "$ORIG_FILE"
 fi
